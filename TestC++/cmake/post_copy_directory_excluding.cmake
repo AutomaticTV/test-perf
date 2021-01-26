439c16e0509d1a9168e8f0b5c7248b21ef51e200
Michal Kucharski <mkucharski@ext.mediapro.tv>
2021-01-26 12:47:10 +0100

    Simplified TestC++ to remove all custom CMake scripts

--- TestC++/cmake/post_copy_directory_excluding.cmake
+++
@@ -1,4 +0,0 @@
-# Script to copy directory to a destination excluding file matching a pattern
-# Params: ${IN_DIR} ${OUT_DIR} ${EXCLUSION_PATTERN}
-
-file(COPY ${IN_DIR} DESTINATION ${OUT_DIR} PATTERN ${EXCLUSION_PATTERN} EXCLUDE)
